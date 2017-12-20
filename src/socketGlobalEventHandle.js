5705332bc3f572726f5be0d76674b4af6236bafc
炒饭 <chaofan@2dfire.com>
2017-12-20 11:47:12 +0800

    eslint fix

--- src/socketGlobalEventHandle.js
+++ src/socketGlobalEventHandle.js
@@ -4,7 +4,7 @@ const log = debug('socket.io-wxapp-client:socketGlobalEventHandle');
 let isInitSocketGlobalEvent = false;
 
 const defaultGloableEventHandler = (...args) => {
-    log('没有socket全局处理事件 %O', args);
+  log('没有socket全局处理事件 %O', args);
 };
 
 // 全局事件接受者
@@ -15,29 +15,29 @@ let gloableEventHandler;
  * @param  {Function} handler 事件接受者
  */
 export default function socketGlobalEventHandle(handler = defaultGloableEventHandler) {
-    // 设置全局事件接受者
-    gloableEventHandler = handler;
+  // 设置全局事件接受者
+  gloableEventHandler = handler;
 
-    if (isInitSocketGlobalEvent) {
-        return;
-    }
-    isInitSocketGlobalEvent = true;
+  if (isInitSocketGlobalEvent) {
+    return;
+  }
+  isInitSocketGlobalEvent = true;
 
-    // 绑定全局监听initListen
-    wx.onSocketOpen(() => {
-        gloableEventHandler('open');
-    });
+  // 绑定全局监听initListen
+  wx.onSocketOpen(() => {
+    gloableEventHandler('open');
+  });
 
-    wx.onSocketError((res) => {
-        gloableEventHandler('error', res);
-    });
+  wx.onSocketError((res) => {
+    gloableEventHandler('error', res);
+  });
 
-    wx.onSocketClose(() => {
-        gloableEventHandler('close');
-    });
+  wx.onSocketClose(() => {
+    gloableEventHandler('close');
+  });
 
-    wx.onSocketMessage((res) => {
-        log('message', res);
-        gloableEventHandler('message', res);
-    });
+  wx.onSocketMessage((res) => {
+    log('message', res);
+    gloableEventHandler('message', res);
+  });
 }
