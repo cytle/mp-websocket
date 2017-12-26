78e83b9d4a6c5b55045aa712cf454f6027125785
炒饭 <chaofan@2dfire.com>
2017-12-26 14:13:28 +0800

    style

--- src/connectSocket.js
+++ src/connectSocket.js
@@ -1,4 +1,7 @@
+import debug from 'debug';
 import socketGlobalEventHandle from './socketGlobalEventHandle';
+
+const log = debug('socket.io-wxapp-client:connectSocket');
 
 function socketEventHandle(handler, socketTask) {
   socketTask.onOpen(() => {
@@ -20,12 +23,23 @@ function socketEventHandle(handler, socketTask) {
 
 let globalWebsocket;
 
+/**
+ * 创建websocket链接，支持旧版本全局实例和新版本多实例
+ * @param  {Object} options   websocket参数
+ * @param  {Object} instance  当前实例
+ * @param  {Function} handler 事件响应
+ * @return {Object}           rocketTask { send, close },如果是旧版本,则降级为全
+ * 局方法
+ */
 export default function connectSocket(options, instance, handler) {
   let socketTask = wx.connectSocket(options);
   if (socketTask) {
+    log('single, 有返回socketTask, 多socket');
     socketEventHandle(handler, socketTask);
   } else {
+    log('global, 单socket');
     if (globalWebsocket) {
+      // 摧毁之前全局实例 TODO 测试
       globalWebsocket.carsh();
     }
     globalWebsocket = instance;
