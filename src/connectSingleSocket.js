d0f3be9c47e195d38d8867d7dba7df92cc9f001e
炒饭 <chaofan@2dfire.com>
2018-03-16 18:45:41 +0800

    fix connect

--- src/connectSingleSocket.js
+++ src/connectSingleSocket.js
@@ -33,34 +33,49 @@ export function hasSingleSocket() {
   return !!globalWebsocket;
 }
 
-export default function connectSingleSocket(instance) {
-  if (globalWebsocket) {
-    if (nextGlobalWebsocket) {
-      log('nextGlobalWebsocket被跳过');
-      nextGlobalWebsocket = instance;
-    } else {
-      log('nextGlobalWebsocket将在当前socket断开后被连接');
-      const { onclose } = globalWebsocket;
-      nextGlobalWebsocket = instance;
-      globalWebsocket.onclose = function close(res) {
-        log('当前websocket断开连接');
-        if (onclose) {
-          onclose.call(this, res);
-        }
-        log('websocket onclose执行完毕');
-        if (nextGlobalWebsocket) {
-          log('nextGlobalWebsocket将连接');
-          setGlobalSocket(nextGlobalWebsocket);
-          wx.connectSocket(nextGlobalWebsocket.$options);
-          nextGlobalWebsocket = undefined;
-        }
-      };
-    }
-  } else {
+function popGlobal() {
+  setGlobalSocket(nextGlobalWebsocket);
+  wx.connectSocket(nextGlobalWebsocket.$options);
+  nextGlobalWebsocket = undefined;
+}
+
+function connect(instance) {
+  if (nextGlobalWebsocket) {
+    log('nextGlobalWebsocket被跳过');
+    nextGlobalWebsocket = instance;
+    return;
+  }
+  nextGlobalWebsocket = instance;
+
+  if (!globalWebsocket) {
     log('websocket将连接');
-    setGlobalSocket(instance);
-    wx.connectSocket(instance.$options);
+    popGlobal();
+    return;
   }
 
+  if (globalWebsocket.readyState === 3) {
+    log('当前socket为断开状态，将打开新socket');
+    popGlobal();
+    return;
+  }
+
+  log('nextGlobalWebsocket将在当前socket断开后被连接');
+  const { onclose } = globalWebsocket;
+  globalWebsocket.onclose = function close(res) {
+    log('当前websocket断开连接');
+    if (onclose) {
+      onclose.call(this, res);
+    }
+    log('websocket onclose执行完毕');
+    if (nextGlobalWebsocket) {
+      log('nextGlobalWebsocket将连接');
+      popGlobal();
+    }
+  };
+  globalWebsocket.close();
+}
+
+export default function connectSingleSocket(instance) {
+  connect();
   return createSingleSocketTask(instance);
 }
